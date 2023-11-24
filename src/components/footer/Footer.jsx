46ce03c133a1b57949a2c6813dce5d2903e2df70
Dupont <zeshin59@gmail.com>
2023-11-24 21:57:18 +0100

    add footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import Classes from './Footer.module.css';
+
+function Footer() {
+    return (
+        <footer class={Classes.footer}>
+            <p class={Classes.footerText}>Copyright 2020 Argent Bank</p>
+        </footer>
+    );
+}
+
+export default Footer;
