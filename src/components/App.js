cf63804ec9f572389d30f6dc6b5815feaec03363
Dupont <zeshin59@gmail.com>
2023-11-25 20:35:11 +0100

    add react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import Accueil from './accueil/Accueil';
 
 function App() {
     return (
-        <>
-        </>
+        <Routes>
+            <Route path='/' element={<Accueil />} />
+        </Routes>
     );
 }
 
